0c9efcd95ec4bff643bacd14b9aff38cd6f6a520
Angelito De Sagun <desagunangelito@gmail.com>
2022-11-10 23:38:25 +0800

    add search and sort functionality

--- main.js
+++ main.js
@@ -6,27 +6,44 @@
 
 //newsList is the variable that holds an array of strings
 export var newsList = [
-  "What films should an aspiring journalist watch?",
-  "Buried underpants and tea bags help scientists evaluate soil",
-  "Decoder: Mining asteroids for minerals can help spare Earth",
-  "Media glare can enrich tennis pros yet imperil mental health",
-  "'Nightmare' TV show 'Euphoria — health threat or high art?",
-  "Decoder: Armenia in a bind as Ukraine war resets global order",
-  "What books should an aspiring journalist read?",
-  "Marie Colvin shined a light on war-torn corners of the world",
+    "What films should an aspiring journalist watch?",
+    "Buried underpants and tea bags help scientists evaluate soil",
+    "Decoder: Mining asteroids for minerals can help spare Earth",
+    "Media glare can enrich tennis pros yet imperil mental health",
+    "'Nightmare' TV show 'Euphoria — health threat or high art?",
+    "Decoder: Armenia in a bind as Ukraine war resets global order",
+    "What books should an aspiring journalist read?",
+    "Marie Colvin shined a light on war-torn corners of the world",
 ];
 
-export function search() {
-  //inputValue is the variable that contains the search string
-  const inputValue = document.getElementById("search-input").value;
+export function search(inputValue = null) {
+    //inputValue is the variable that contains the search string
+    if(inputValue == null){
+      inputValue = document.getElementById("search-input").value;
+    }
 
-  //Write your code here for the search function
+
+    //Write your code here for the search function
+    let newList = [];
+
+    newsList.forEach(element => {
+        if(element.toLowerCase().indexOf(inputValue.toLowerCase()) > -1){
+            newList.push(element);
+        }
+    });
+
+    return newList;
 }
 
 export function sort(type) {
-  if (type == "ascending") {
-    //Write your code here for sorting (ascending)
-  } else {
-    //Write your code here for sorting (descending)
-  }
+    if (type == "ascending") {
+      //Write your code here for sorting (ascending)
+      newsList.sort();
+    } else {
+      //Write your code here for sorting (descending)
+      newsList.sort();
+      newsList.reverse();
+    }
+
+    return newsList;
 }
--- main.test.js
+++ main.test.js
@@ -1,6 +1,45 @@
 import { sort, search, newsList } from "./main.js";
 
 //Example testing for search function
-test("Search function testing", () => {
-  expect(search(newsList)).toBe("I am a cool web developer");
+// test("Search function testing", () => {
+//   expect(search(newsList)).toBe("I am a cool web developer");
+// });
+
+test("Search function testing exact.", () => {
+    expect(search("'Nightmare' TV show 'Euphoria — health threat or high art?"))
+    .toEqual(["'Nightmare' TV show 'Euphoria — health threat or high art?"]);
 });
+
+test("Search function with keyword.", () =>{
+    expect(search("What")).toEqual([
+        "What films should an aspiring journalist watch?",
+        "What books should an aspiring journalist read?"
+    ]);
+});
+
+test("Sort news list function ascending testing" ,() =>{
+    expect(sort("ascending")).toEqual([
+        "'Nightmare' TV show 'Euphoria — health threat or high art?",
+        'Buried underpants and tea bags help scientists evaluate soil',
+        'Decoder: Armenia in a bind as Ukraine war resets global order',
+        'Decoder: Mining asteroids for minerals can help spare Earth',
+        'Marie Colvin shined a light on war-torn corners of the world',
+        'Media glare can enrich tennis pros yet imperil mental health',
+        'What books should an aspiring journalist read?',
+        'What films should an aspiring journalist watch?'
+    ]);
+});
+
+test("Sort news list function descending testing" ,() =>{
+    expect(sort("descending")).toEqual([
+        'What films should an aspiring journalist watch?',
+        'What books should an aspiring journalist read?',
+        'Media glare can enrich tennis pros yet imperil mental health',
+        'Marie Colvin shined a light on war-torn corners of the world',
+        'Decoder: Mining asteroids for minerals can help spare Earth',
+        'Decoder: Armenia in a bind as Ukraine war resets global order',
+        'Buried underpants and tea bags help scientists evaluate soil',
+        "'Nightmare' TV show 'Euphoria — health threat or high art?"
+    ]);
+});
+
